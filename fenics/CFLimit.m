16ce37957ea649862794868dbdcac002006d046e
peteruhl2 <59039799+peteruhl2@users.noreply.github.com>
2022-09-26 12:38:27 -0700

    worked on CF limit

--- fenics/CFLimit.m
+++ fenics/CFLimit.m
@@ -1,25 +1,25 @@
 %%% simple CF ODE system with limit cycles
 %%% started 9/10/22
 
-close all
+% close all
 
 global beta b n dc df q mu lambda eta k
 
 k = 100*100;
-beta = 2.;
+beta = 2.0;
 b = 1.;
 n = 1.0;
 dc = 9e-1;
 df = 9e-1;
-q = 4.48e-0*0;
+q = 4.48e-4;
 mu = 0.001;
-lambda = 0.002*k;
-eta = .054;
+lambda = 0.002;
+eta = 5.4e-6;
 
 
 c0 = 100.4;
-f0 = 20.3;
-w0 = 0.1;
+f0 = 2.3;
+w0 = 1e-5;
 y0 = [c0; f0; w0];
 
 tspan = [0, 5000];
@@ -41,6 +41,12 @@ plot(t, y(:,3), 'linewidth',2)
 xlabel('Time')
 legend('Oxygen','Fontsize',18)
 
+% hold on; box on
+% plot(y(:,1),y(:,2),'linewidth',1)
+% scatter(y(end,1),y(end,2))
+% xlabel('C')
+% ylabel('F')
+
 
 
 
@@ -53,7 +59,7 @@ global beta b n dc df q mu lambda eta k
 
 c = y(1);
 f = y(2);
-w = y(3)/k;
+w = y(3);
 
 yp(1) = (beta*w^n)/(b^n + w^n)*c*(1-(c+f)/k) - dc*c;
 yp(2) = beta*(1 - (w^n)/(b^n + w^n))*f*(1-(c+f)/k) - df*f - q*f*w;
