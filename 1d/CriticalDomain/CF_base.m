df65dbde3c27dc0849ccc6cf3b12261061badf3c
peteruhl2 <59039799+peteruhl2@users.noreply.github.com>
2022-09-26 12:10:36 -0700

    some stuff

--- 1d/CriticalDomain/CF_base.m
+++ 1d/CriticalDomain/CF_base.m
@@ -2,25 +2,26 @@
 %%%
 %%% started 9/22/2022
 
-global beta dc df mu eta local_lambda Dc Df Dw L
+global beta dc df mu eta local_lambda Dc Df Dw L q
 
-beta = 2.0;
+beta = 2.20;
 dc = 0.01;
 df = 0.01;
 
 local_lambda = 1.0;
 mu = 1;
-eta = 0;
+eta = 1.0;
+q = 5.70;
 
-Dc = 4e-1;
+Dc = 4e-4;
 Df = 4e-1;
-Dw = 4e-1;
+Dw = 4.0e-0;
 
 %%% Domain
-L = 5.7;
-x = linspace(-L,L);
+L = 2.;
+x = linspace(-L,L,200);
 
-tmax = 20;
+tmax = 500;
 t = linspace(0,tmax);
 dt = tmax/(length(t));
 
@@ -30,24 +31,43 @@ sol = pdepe(m, @rhs, @icfun, @bcs, x, t);
 
 %%% Plots =================================================================
 
-% % ---- Plot the solution --- %
-% step=1;  %step every second
-% for tt=1:step:length(t)
-% % disp(sprintf('time = %d',(tt-1)*dt));
-% disp((sprintf('time = %.02f out of %d',tt*dt,tmax)));
-% plot(x,sol(tt,:));
-% % legend('c','f','w');
-% % ylim([-0.1,lambda/mu]);
-% ylim([-0.1,1.1]);
-% drawnow;
-% %     pause(0.0025);
-% end
-% % -------------------------- %
+% ---- Plot the solution --- %
+step=1;  %step every second
+for tt=1:step:length(t)
+% disp(sprintf('time = %d',(tt-1)*dt));
+disp((sprintf('time = %.02f out of %d',tt*dt,tmax)));
+plot(x,sol(tt,:,1),x,sol(tt,:,2),x,sol(tt,:,3));
+% legend('c','f','w');
+% ylim([-0.1,lambda/mu]);
+ylim([-0.1,1.1]);
+% axis tight
+drawnow;
+%     pause(0.0025);
+end
+close
+% -------------------------- %
 
 
 
 [X,T] = meshgrid(x,t);
 
+%%% aerobes
+figure()
+contourf(X,T,sol(:,:,1))
+xlabel('x')
+ylabel('t')
+colorbar
+title('Aerobes')
+
+%%% anaerobes
+figure()
+contourf(X,T,sol(:,:,2))
+xlabel('x')
+ylabel('t')
+colorbar
+title('Anaerobes')
+
+%%% oxygen
 figure()
 contourf(X,T,sol(:,:,3))
 xlabel('x')
@@ -69,7 +89,7 @@ title('Oxygen')
 
 %%% RHS PDE function
 function [c,f,s] = rhs(x,t,u,dudx)
-global beta dc df mu eta Dc Df Dw L local_lambda
+global beta dc df mu eta Dc Df Dw L local_lambda q
 
 %%% assign spatial lambda value
 if abs(x) > 0.9*L
@@ -82,8 +102,9 @@ c = [1; 1; 1];
 f = [Dc*dudx(1); Df*dudx(2); Dw*dudx(3)];
 
 s = [(beta*u(3)/(1 + u(3)))*u(1)*(1 - u(1) - u(2)) - dc*u(1);
-     (beta*(1 - u(3)/(1+u(3))))*u(2)*(1 - u(1) - u(2)) - df*u(2);
+     (beta*(1 - u(3)/(1+u(3))))*u(2)*(1 - u(1) - u(2)) - df*u(2) - q*u(2)*u(3);
      l - mu*u(3) - eta*u(1)*u(3)];
+
 end
 
 %%% IC function
@@ -91,7 +112,9 @@ function u0 = icfun(x)
 
 % u0 = exp(-(x).^2);
 
-u0 = [1; 1; exp(-(x).^2)];
+u0 = [0.4*exp(-(x).^2); 
+      0.2*exp(-(x).^2); 
+      1 - exp(-(x).^2)];
 
 
 end
@@ -99,18 +122,18 @@ end
 %%% BC function
 function [pl,ql,pr,qr] = bcs(xl,ul,xr,ur,t)
 
-%%% Diriclet ul = ur = 0 for c,f
-%%% oxygen is no flux
-pl = [ul(1); ul(2); 0];
-ql = [0; 0; 1];
-pr = [ur(1); ur(2); 0];
-qr = [0; 0; 1];
+% %%% Diriclet ul = ur = 0 for c,f
+% %%% oxygen is no flux
+% pl = [ul(1); ul(2); 0];
+% ql = [0; 0; 1];
+% pr = [ur(1); ur(2); 0];
+% qr = [0; 0; 1];
 
-% %%% No flux
-% pl = 0;
-% ql = 1;
-% pr = 0;
-% qr = 1;
+%%% No flux
+pl = [0; 0; 0];
+ql = [1; 1; 1];
+pr = [0; 0; 0];
+qr = [1; 1; 1];
 
 end
 
